72927aeb35aaca8b5bf8820643a0cb9cf9e8335e
Eduardo Nicastro <enicastro@fullstacklabs.co>
2021-11-07 13:45:00 -0300

    add comment form to post page

--- app/pages/posts/[postId].tsx
+++ app/pages/posts/[postId].tsx
@@ -3,12 +3,17 @@ import { Head, Link, useRouter, useQuery, useParam, BlitzPage, useMutation, Rout
 import Layout from "app/core/layouts/Layout"
 import getPost from "app/posts/queries/getPost"
 import deletePost from "app/posts/mutations/deletePost"
+import createComment from "app/comments/mutations/createComment"
+import deleteComment from "app/comments/mutations/deleteComment"
+import { CommentForm, FORM_ERROR } from "app/comments/components/CommentForm"
 
 export const Post = () => {
   const router = useRouter()
   const postId = useParam("postId", "number")
+  const [createCommentMutation] = useMutation(createComment)
   const [deletePostMutation] = useMutation(deletePost)
-  const [post] = useQuery(getPost, { id: postId })
+  const [deleteCommentMutation] = useMutation(deleteComment)
+  const [post, { refetch }] = useQuery(getPost, { id: postId })
 
   return (
     <>
@@ -18,13 +23,6 @@ export const Post = () => {
 
       <div>
         <h1>{post.text}</h1>
-        <ul>
-          {post.comments.map((comment) => (
-            <li key={comment.id}>
-              {comment.text} - {comment.votes} votes
-            </li>
-          ))}
-        </ul>
 
         <Link href={Routes.EditPostPage({ postId: post.id })}>
           <a>Edit</a>
@@ -42,6 +40,50 @@ export const Post = () => {
         >
           Delete
         </button>
+
+        <h3>Comments</h3>
+        <ul>
+          {post.comments.map((comment) => (
+            <li key={comment.id}>
+              {comment.text} - {comment.votes} votes
+              <span style={{ marginLeft: "1rem" }}>
+                <Link href={Routes.EditCommentPage({ commentId: comment.id })}>
+                  <a>Edit</a>
+                </Link>
+                <button
+                  type="button"
+                  onClick={async () => {
+                    if (window.confirm("This will be deleted")) {
+                      await deleteCommentMutation({ id: comment.id })
+                      router.push(Routes.ShowPostPage({ postId: post.id }))
+                    }
+                  }}
+                  style={{ marginLeft: "0.5rem" }}
+                >
+                  Delete
+                </button>
+              </span>
+            </li>
+          ))}
+        </ul>
+        <CommentForm
+          submitText="Add Comment"
+          onSubmit={async (values) => {
+            try {
+              await createCommentMutation({
+                ...values,
+                postId: post.id,
+              })
+              refetch()
+              values.text = ""
+            } catch (error: any) {
+              console.error(error)
+              return {
+                [FORM_ERROR]: error.toString(),
+              }
+            }
+          }}
+        />
       </div>
     </>
   )
